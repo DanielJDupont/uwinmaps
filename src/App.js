38bbb4dc80a446550b7fff49260913281e99bd91
Dan <ddupont2@uwo.ca>
2019-09-26 23:38:43 -0400

    first commit!

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://images.pexels.com/photos/1663421/pexels-photo-1663421.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt="Puppy wearing a crown"
+        ></img>
+        <p>Welcome to UWinMaps!</p>
+        <p>Find Your Class Today!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
