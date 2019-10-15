bafd6d62a6743b0f3b98698fda300acc220b77be
andrealswa <swartz8@uwindsor.ca>
2019-10-15 12:57:07 -0400

    minor changes

--- src/main/navmenu/home/home.jsx
+++ src/main/navmenu/home/home.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 
 // Components
-import MainCarousel from "./maincarousel/maincarousel";
-import InfoJumbotron from "./infojumbotron/infojumbotron";
-import Accordion from "./accordion/accordian";
-import RoomSearch from "./roomsearch/roomsearch";
+import MainCarousel from './maincarousel/maincarousel';
+import InfoJumbotron from './infojumbotron/infojumbotron';
+import Accordion from './accordion/accordian';
+import RoomSearch from './roomsearch/roomsearch';
+// import SpringChain from './springchain/springchain';
 
 // JSON
-import buildingsAndRooms from "./buildingsandrooms.json";
+import buildingsAndRooms from './buildingsandrooms.json';
 
 function HomePage() {
   return (
