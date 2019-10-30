5c45a13c3f14e5e571ac655ed50474768451406e
andrealswa <swartz8@uwindsor.ca>
2019-10-29 20:38:02 -0400

    I added a new component using the Google Maps API with the npm package, google-map-react and labelled some of the university buildings

--- src/components/navmenu/home/home.jsx
+++ src/components/navmenu/home/home.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react';
 
 // Components
-import MainCarousel from "./maincarousel/maincarousel";
-import InfoJumbotron from "./infojumbotron/infojumbotron";
-import Accordion from "./accordion/accordian";
-import RoomSearch from "./roomsearch/roomsearch";
-import RoomAdder from "./roomadder/form";
+import SimpleMap from '../outdoormap/outdoormap';
+import MainCarousel from './maincarousel/maincarousel';
+import InfoJumbotron from './infojumbotron/infojumbotron';
+import Accordion from './accordion/accordian';
+import RoomSearch from './roomsearch/roomsearch';
+import RoomAdder from './roomadder/form';
 
 // import SpringChain from './springchain/springchain';
 
 function HomePage() {
   return (
     <div>
+      <SimpleMap />
       <MainCarousel />
       <InfoJumbotron />
       <RoomAdder />
