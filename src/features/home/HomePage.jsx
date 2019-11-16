21305e4f98e8053a9527ddcc0aa2d1a4c166ea2a
Dan <ddupont2@uwo.ca>
2019-11-16 02:12:45 -0500

    confetti

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -8,9 +8,12 @@ import {
   Header
 } from "semantic-ui-react";
 
+import Confetti from "react-confetti";
+
 const HomePage = () => {
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
+      <Confetti numberOfPieces={3} colors={["yellow"]} opacity={0.2} />
       <Container text>
         <Header as="h1" inverted>
           <Icon name="yellow large map outline" inverted />
