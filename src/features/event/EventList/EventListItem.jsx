0e8ab7d4d10fd1e302ff409c76f241d7e6bf8c4e
Dan <ddupont2@uwo.ca>
2019-11-27 00:33:35 -0500

    changed from orange to red, just looks more consistent

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -36,7 +36,7 @@ class EventListItem extends Component {
         </Segment>
         <Segment>
           <span>
-            <Icon className="orange" name="clock" />
+            <Icon className="red" name="clock" />
             {format(event.date.toDate(), "EEEE do LLL")} at{" "}
             {format(event.date.toDate(), "h:mm a")} |
             <Icon className="red" name="marker" /> {event.venue}
