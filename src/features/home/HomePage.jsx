d5b65b10a26f7eaec60272d893155d463bc58aa6
Dan <ddupont2@uwo.ca>
2019-12-02 23:47:06 -0500

    fire changes

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -15,7 +15,7 @@ import useWindowDimensions from "./WindowHook";
 
 const HomePage = ({ history }) => {
   const { height, width } = useWindowDimensions();
-  console.log(height);
+
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Confetti
@@ -23,7 +23,8 @@ const HomePage = ({ history }) => {
         colors={["yellow"]}
         opacity={0.15}
         confettiSource={{ x: 0, y: height, w: 3000, h: 0 }}
-        gravity={-0.15}
+        gravity={-0.1}
+        wind={0}
       />
       <Container text>
         <Header as="h1" inverted>
